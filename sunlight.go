8b4acc74f8d286f9344dbe133418aaaa67c61824
Brian Buller <brian@bullercodeworks.com>
2016-06-17 13:43:20 -0500

    Sunlight Interface

--- sunlight.go
+++ sunlight.go
@@ -1,1 +1,9 @@
 package sunlightApi
+
+import "net/url"
+
+// SunlightAPI is the interface for all of the
+// Sunlight Foundation's APIs
+type SunlightAPI interface {
+	call(string, url.Values) ([]byte, error)
+}
